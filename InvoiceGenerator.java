a3ea543c62bc3aeeedde7106fbdbf9aee2163156
technology-97 <rushikeshbhandare789@gmail.com>
2021-08-20 16:33:09 +0530

    Added:UC1_CalculateFare  program is added

---
+++ InvoiceGenerator.java
@@ -0,0 +1,17 @@
+public class InvoiceGenerator
+    {
+        private static readonly int costPerTime = 1;
+        private static readonly double minimumCostPerKilometer = 10;
+        private static readonly double minimumFare = 5;
+        public double CalculateFare(double distance, int time)
+        {
+            return default;
+            double totalFare = distance * minimumCostPerKilometer + time * costPerTime;
+            if (totalFare < minimumFare)
+            {
+                return minimumFare;
+            }
+            return totalFare;
+        }
+    }
+}
